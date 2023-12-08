ae9157abdd96686cc802d062cf89f64d2f57eca1
luis antonio <tyluis20@gmail.com>
2023-12-08 11:36:37 -0300

    minha solucao cipher

--- basics_concepts/lambda_and_enumerable.rb
+++ basics_concepts/lambda_and_enumerable.rb
@@ -37,3 +37,35 @@ friends.each { |friend| shouting_at_friends.push(friend.upcase) }
 
 my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
 my_order.map { |item| item.gsub('medium', 'extra large') }
+
+
+#none
+
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+matches = []
+result = false
+
+fruits.each do |fruit|
+  if fruit.length > 10
+    matches.push(fruit)
+  end
+
+  result = matches.length == 0
+end
+
+result
+#=> true
+
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+matches = []
+result = false
+
+fruits.each do |fruit|
+  if fruit.length > 6
+    matches.push(fruit)
+  end
+
+  result = matches.length == 0
+end
+
+result
--- cyphers/caesar_cipher.rb
+++ cyphers/caesar_cipher.rb
@@ -1,9 +1,43 @@
-#a cifra de ceaser consiste que apartir de um valor de uma chave 
-# modificamos o a letra que consite naquela posiçãp do alfabeto 
 # exemplo
 #key = 5
-#entao
 #'A' + 5 = 'F'
-caesar_cipher("What a string!", 5)
+#solution 1
 
-result = "Bmfy f xywnsl!"
+def cipher(string, shift = 1) 
+  alfabeto = ("a".."z").to_a
+  ceaser =  ""
+  string.each_char do |letra|
+    cap = letra.match(/[A-Z]/) 
+    if letra.ord < 64 
+      ceaser += letra
+    else 
+      idx = alfabeto.find_index(letra.downcase).to_i
+      new_idx = (idx + shift ) % 26
+      ceaser += cap ? alfabeto[new_idx].upcase : alfabeto[new_idx]
+    end
+  end
+  ceaser
+end 
+
+
+#prova >>>
+def caesar_cipher(string, shift)
+  new_string = string.split('').map do |char|
+    if char.match(/[a-z]/)
+      char = char.ord + shift
+      char = (char - 122) + 96 until char < 123
+      char.chr
+    elsif char.match(/[A-Z]/)
+      char = char.ord + shift
+      char = (char - 90) + 64 until char < 91
+      char.chr
+    else char 
+    end
+  end
+  return new_string.join("")
+end
+
+puts cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 5)
+
+
